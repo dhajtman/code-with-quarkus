06a8a2aca7953da5ffd69b9cb5f767627eacf80d
Dusan <dusan@me.com>
2025-04-08 12:29:30 +0200

    added postgres

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,8 @@
 -- insert into myentity (id, field) values(2, 'field-2');
 -- insert into myentity (id, field) values(3, 'field-3');
 -- alter sequence myentity_seq restart with 4;
+
+INSERT INTO known_fruits(id, name) VALUES (1, 'Cherry');
+INSERT INTO known_fruits(id, name) VALUES (2, 'Apple');
+INSERT INTO known_fruits(id, name) VALUES (3, 'Banana');
+ALTER SEQUENCE known_fruits_id_seq RESTART WITH 4;
